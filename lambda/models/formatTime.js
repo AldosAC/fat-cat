47e68a30cb6a6be9dcfe77adf38e4ff60596d847
Joel Carpenter <aldosac@gmail.com>
2020-09-15 11:58:46 -0700

    Changes "hours" property to "hour" in formatTime.js

--- lambda/models/formatTime.js
+++ lambda/models/formatTime.js
@@ -1,14 +1,14 @@
 module.exports.formatTime = (timeStamp) => {
-  let hours = timeStamp.getHours();
+  let hour = timeStamp.getHours();
   let minutes = timeStamp.getMinutes();
   let amPm;
 
   if (hours > 12) {
-    hours = hours - 12;
+    hour = hour - 12;
     amPm = "PM";
   } else {
     amPm = "AM";
   }
 
-  return { hours, minutes, amPm };
+  return { hour, minutes, amPm };
 }
